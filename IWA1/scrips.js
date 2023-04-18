64ffed5be6235720357cb41f5732be017951ff2a
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-04-18 18:23:24 +0200

    modified:   IWA1/index.html         modified:   IWA1/scrips.js         new file:   IWA2/Challenge 1.js         new file:   IWA2/Challenge 2.js         new file:   IWA2/Challenge 3.js

--- IWA1/scrips.js
+++ IWA1/scrips.js
@@ -1,12 +1,13 @@
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
+const STEP_AMOUNT = 5
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
 const subtractHandler = () => {
-    const newValue = parseInt(number.value) - 1
+    const newValue = parseInt(number.value) - STEP_AMOUNT
     number.value = newValue
 
     if (add.disabled === true)
@@ -18,7 +19,7 @@ const subtractHandler = () => {
 }
 
 const addHandler = () => {
-    const newValue = parseInt(number.value) + 1
+    const newValue = parseInt(number.value) + STEP_AMOUNT
     number.value = newValue
 
     if (subtract.disabled === true)
