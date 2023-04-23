e8c0b7cfd5bdd954b1ebe575bd652b8bda9c4f96
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-04-23 20:33:05 +0200

    new file:   IWA5/Challenge 1.js

---
+++ IWA5/Challenge 1.js
@@ -0,0 +1,41 @@
+FREE_WARNING = 'Free shipping only applies to single customer orders'
+BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+NONE_SELECTED = 0
+
+let customers = 1
+const location = "AUS"
+currency = "$"
+
+shipping = null
+
+const RSA = "RSA"
+const NAM = "NAM"
+
+if (location === RSA) { 
+	shipping = 400  
+	currency === 'R' 
+} else if (location == NAM){
+	shipping = 600 
+}
+else shipping = 800
+
+shoes = 300 * 1
+toys = 100 * 5
+shirts = 150 * NONE_SELECTED
+batteries = 35 * 2
+pens = 5 * NONE_SELECTED
+
+let price = shoes + toys + shirts + batteries + pens
+
+if (price > 1000) {
+	if (location === NAM && customers < 2) {
+			if (location === RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
+
+if (shipping === 400 && customers !== 1) { 
+	console.log(FREE_WARNING) 
+}
+
+location === 'NK' ? console.log(BANNED_WARNING) : console.log('price', currency, price + shipping)
