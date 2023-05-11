e21d8d695ec479aacf713490bbe925aa2867be64
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-05-11 18:16:27 +0200

    modified:   IWA9/Challenge 9.js

--- IWA9/Challenge 9.js
+++ IWA9/Challenge 9.js
@@ -5,24 +5,24 @@ const size = 'large'
 
 // Only change the syntax below (not the values or key names)
 
-const expenses = {
+const expenses = {                                                                         //PROPERTIES
     food: 51.7501,
-    transport: 10.2,                                                                        //PROPERTIES
+    transport: 10.2,                                                                        
 }
   
-const tax = {
+const tax = {                                                                              //PROPERTIES
     734: '3%',
     234: '20%',
-    913: '12%',                                                                              //PROPERTIES
+    913: '12%',                                                                             
     415: '38%',
     502: '42%',
 }
 
-const rent = {
+const rent = {                                                                             //PROPERTIES
     none: 0,
     'small-room': 200,
     'large-room': 300,
-    'small-apartment': 400,                                                                  //PROPERTIES
+    'small-apartment': 400,                                                                  
     'large-apartment': 800,
     'small-house': 1200,
     'large-house': 2400,
@@ -30,8 +30,8 @@ const rent = {
 
 // You can change below however you want
 
-const taxAsDecimal = parseFloat(tax[913]) / 100;                                            //parsefloat tax value 913, you need the % in decimal
-const startingAfterTax = salary * (1 - taxAsDecimal);                                       //put 1 - Decimal value in brackets it should first calculate 1 - the taxdecimal then times salary
-const type = size + '-' + lodging;                                                          //Swop around Wording // Put a -  in the middle of words to form the word 'Large-Apartment'.
-const balance = startingAfterTax - expenses.transport - expenses.food - rent[type];         //Add Sallary, Expenses in brackets should be a dot to access the statement of expenses //Remove expenses for rent 
-console.log(type, balance.toFixed(2));                                                      //add tofixes for 2 decimal for answer.
+const taxAsDecimal = parseFloat(tax[913]) / 100                                            //parsefloat tax value 913, you need the % in decimal
+const startingAfterTax = salary * (1 - taxAsDecimal)                                       //put 1 - Decimal value in brackets it should first calculate 1 - the taxdecimal then times salary
+const type = size + '-' + lodging                                                          //Swop around Wording // Put a -"-" in the middle of words to form the word 'Large-Apartment'.
+const balance = startingAfterTax - expenses.transport - expenses.food - rent[type]         //Add Sallary, Expenses in brackets should be a dot to access the statement of expenses //Remove expenses for rent 
+console.log(type, balance.toFixed(2))                                                      //add tofixes for 2 decimal for answer.
