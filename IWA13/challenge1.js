048fca8914cb7d89c37aa1f3f49fc6c41a5aa7e8
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-05-27 20:44:44 +0200

    modified:   IWA11/Challenge 1/script.js         modified:   IWA13/challenge1.js         new file:   IWA14/Notes.text

--- IWA13/challenge1.js
+++ IWA13/challenge1.js
@@ -4,25 +4,30 @@ let calculated = '1'
 
 // Only allowed to change below
 
+let isLogged = false                                                               //New flag variable
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
-}
+const logCalc = () => {                                                            // "=" on the wong side 
+    const isString = typeof calculated === 'numerical-string';                     // add stricked "==="
+    const calculatedAsNumber = isString ?  calculated: parseInt(calculated);        //Convert string to number "parseInt"
+    calculated = calculatedAsNumber + 1
+};
 
-const = calcUser () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
-}
+const calcUser = () => {                                                // "=" on the wong side 
+   logCalc()                                                            //Add bracket to enable function "logCalc"                   
+  if (calculated > 2) {user = 'John'};
+  if (calculated > 2 && isLogged === false)                             // Check if it hasn't been logged yet
+    {state = 'requesting';
+    isLogged = true;                                                    // Set the flag to true after logging
+    console.log(`User: ${user} (${calculated})`)                        // Log the user information
+  }
+  if (calculated >= 3) state = 'idle';                                 //add "=" sign
+};
 
-const = checkUser () => {
+const checkUser = () => {                                              // "=" on the wong side 
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
-	}
-}
+  }
+};
 
 // Only allowed to change code above
 
