8cf2f9deaa4c936d27d1ec37b4f93b2c7cb22721
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-06-05 17:58:17 +0200

    modified:   IWA15/Challenge 1.js         new file:   IWA16/Challenge 1/index.html         new file:   IWA16/Challenge 1/scripts.js         new file:   IWA16/Notes.text

--- IWA15/Challenge 1.js
+++ IWA15/Challenge 1.js
@@ -8,19 +8,19 @@ const data = {
 
 // Only edit below
 
-const first = data.lists[0][1] || {}
-const second = data.lists[1][1] || {}
-const third = data.lists[2][1]|| {}
+const first = data.lists[0][1] || {}                      //decalre First, second and Third: 
+const second = data.lists[1][1] || {}					  //get the data information
+const third = data.lists[2][1]|| {}						 //call the Array by using "[ ]" keep in mind Array always start at 0
 
 const result = []
 
 const extractBiggest = () => {
-	let firstLast = first[first.length -1]
-	let secondLast = second[second.length -1]
+	let firstLast = first[first.length -1]                //use let to declare firstlast, secondlast and thirdlast
+	let secondLast = second[second.length -1]			  //add first.length -1 to check number from array's from the back of the 3 lines.
 	let thirdLast = third[third.length -1]
 
-	if (firstLast > secondLast && firstLast > thirdLast) {
-		return first.pop()
+	if (firstLast > secondLast && firstLast > thirdLast) {      // use the if statments correctly
+		return first.pop()										//use .pop to remove the number from the back and return the numbers thats left 
 	}
 
 	if (secondLast > thirdLast) {
