723ded96d5a25ef13387e0e75550f98e5c78b1a2
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-06-11 14:20:45 +0200

    modified:   IWA17/Challenge 1/scripts.js         modified:   IWA17/Challenge 1/styles.css

--- IWA17/Challenge 1/scripts.js
+++ IWA17/Challenge 1/scripts.js
@@ -18,86 +18,84 @@ const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 
 // Only edit below 
 
 const createArray = (length) => {
-    const result = []
+    const result = [];
 
-    console.log(result)
-
-    for (let = ; i < length; i) {
-        result.push()
+    for (let i = 0; i < length; i++) {                                    // let "i" =  "0"
+        result.push(i);
     }
 
-    return result
+    return result;
 }
 
-const createData = () > {
-    const current = new ()
-    current.set(1)
+const createData = () => {                                                 // add "="
+    const current = new Date();                                            //ad 'date'
+    current.getMonth();                                                  //add 'get month'
 
-    const startDay = current.get()
-    const daysInMonth = getDaysInMonth(current)
+    const startDay = current.getDate();
+    const daysInMonth = getDaysInMonth(current);
 
-    const weeks = createArray(5)
-    const days = ()
-    const result = []
+    const weeks = createArray(5);
+    const days = createArray(7) ;                                         // 7 days in a week add "createArray"
+    const result = [];
 
     for (const weekIndex of weeks) {
         result.push({
             week: weekIndex + 1,
-            days: []
-        })
+            days: [],
+        });
 
-        for (const  of ) {
-            const day = (dayIndex - startDay) : (weekIndex * 7)  1
-            const isValid = day > 0 && day <= daysInMonth
+        for (const dayIndex of days ) {                                // Day Index of days" 7 days a week
+            const day = (dayIndex - startDay) + (weekIndex * 7) + 8;   // should add "-"
+            const isValid = day > 0 && day <= daysInMonth; 
 
             result[weekIndex].days.push({
-                dayOfWeek: dayIndex + 1,
+                dayOfWeek: dayIndex - 1,
                 value: isValid ? day : '',
-            })
+            });
         }
     }
 
     return result
 }
 
+console.log(createData());
+
 const addCell = (existing, classString, value) => {
     const result = /* html */ `
-        ${}
+        ${existing}
 
         <td class="${classString}">
             &nbsp;${value}&nbsp;
         </td>
-    `
-
+    `;
     return result
-}
+}                                                    //add existing inside curly bracket
 
 const createHtml = (data) => {
-    let result = ''
+    let result = '';
 
-    for (const { week,  } of ) {
-        let inner = ""
+    for (const {week, days} of data) {                                //add "days" and "data"
+        let inner = '';                                                 //change to single quotes
         inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`)
-    
-         (const { dayOfWeek, value } of days) {
-            const isToday = new Date().getDate() === value
-            const isWeekend = dayOfWeek ===  | dayOfWeek === 
-            const isAlternate = week % 2 === 0
-            
-						let classString = 'table__cell'
 
-            if () classString = `${} table__cell_`
-            if () classString = `${} table__cell_`
-            if () classString = `${} table__cell_`
-            inner = addCell()
-        }
+         for (const { dayOfWeek, value} of days) {                     // add "for"
+            const isToday = new Date().getDate() === value;
+            const isWeekend = dayOfWeek === 5 || dayOfWeek === -1;      // add 5 or | or -1 to get
+            const isAlternate = week % 2 === 0;
+
+            let classString = 'table__cell';
+            
+            if (isToday) {classString = `${classString } table__cell_today`;}              //add "()" arround isToday
+            if (isWeekend) {classString = `${classString } table__cell_weekend`;}          //add "()" arround isWeekend
+            if (isAlternate) {classString = `${classString } table__cell_alternate`;}      //add "()" arround isAlternate
+            inner = addCell(inner, classString, value)
+    }
 
         result = `
             ${result}
             <tr>${inner}</tr>
         `
     }
-    
     return result
 }
 
